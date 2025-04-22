de57ddedb8ca80df7190c4eb070d45ec064e487c
Thomas Ridd <twridd@gmail.com>
2025-04-22 06:11:54 -0400

    ready for processing data

--- server.py
+++ server.py
@@ -4,24 +4,93 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 import os
 from worker import speech_to_text, text_to_speech, watsonx_process_message
+# To call watsonx's LLM, we need to import the library of IBM Watson Machine Learning
+from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes
+from ibm_watson_machine_learning.foundation_models import Model
+
+# placeholder for Watsonx_API and Project_id incase you need to use the code outside this environment
+# API_KEY = "Your WatsonX API"
+PROJECT_ID= "skills-network"
+
+# Define the credentials 
+credentials = {
+    "url": "https://us-south.ml.cloud.ibm.com"
+    #"apikey": API_KEY
+}
+    
+# Specify model_id that will be used for inferencing
+model_id = ModelTypes.FLAN_UL2
+
+# Define the model parameters
+from ibm_watson_machine_learning.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watson_machine_learning.foundation_models.utils.enums import DecodingMethods
+
+parameters = {
+    GenParams.DECODING_METHOD: DecodingMethods.GREEDY,
+    GenParams.MIN_NEW_TOKENS: 1,
+    GenParams.MAX_NEW_TOKENS: 1024
+}
+
+# Define the LLM
+model = Model(
+    model_id=model_id,
+    params=parameters,
+    credentials=credentials,
+    project_id=PROJECT_ID
+)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-
 @app.route('/', methods=['GET'])
 def index():
-    return None
-
+    return render_template('index.html')
 
 @app.route('/speech-to-text', methods=['POST'])
 def speech_to_text_route():
-    return None
+    print("processing Speech-to-Text")
+    audio_binary = request.data # Get the user's speech from their request
+    text = speech_to_text(audio_binary) # Call speech_to_text function to transcribe the speech
 
+    # Return the response to user in JSON format
+    response = app.response_class(
+        response=json.dumps({'text': text}),
+        status=200,
+        mimetype='application/json'
+    )
+    print(response)
+    print(response.data)
+    return response
 
 @app.route('/process-message', methods=['POST'])
 def process_message_route():
-    return None
+    user_message = request.json['userMessage'] # Get user's message from their request
+    print('user_message', user_message)
+
+    voice = request.json['voice'] # Get user\'s preferred voice from their request
+    print('voice', voice)
+
+    # Call watsonx_process_message function to process the user's message and get a response back
+    watsonx_response_text = watsonx_process_message(user_message)
+
+    # Clean the response to remove any emptylines
+    watsonx_response_text = os.linesep.join([s for s in watsonx_response_text.splitlines() if s])
+
+    # Call our text_to_speech function to convert Watsonx Api's reponse to speech
+    watsonx_response_speech = text_to_speech(watsonx_response_text, voice)
+
+    # convert watsonx_response_speech to base64 string so it can be sent back in the JSON response
+    watsonx_response_speech = base64.b64encode(watsonx_response_speech).decode('utf-8')
+
+    # Send a JSON response back to the user containing their message\'s response both in text and speech formats
+    response = app.response_class(
+        response=json.dumps({"watsonxResponseText": watsonx_response_text, "watsonxResponseSpeech": watsonx_response_speech}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    print(response)
+    return response
 
 
 if __name__ == "__main__":
